2a4434d1149de25ea480d1da3c7797d4606805b9
Linus <liljalinus@gmail.com>
2017-12-19 00:41:27 +0100

    mysql update

--- mysql.sql
+++ mysql.sql
@@ -6,3 +6,9 @@ CREATE TABLE `jwt-users` (
   `timestamp` varchar(64) NOT NULL DEFAULT '',
   `bio` varchar(1024) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `li-foo` (
+  `id` int(9) NOT NULL,
+  `data` varchar(1024) NOT NULL,
+  `added` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
